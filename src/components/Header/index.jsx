44629160f8c9e45a69d693503db8d80e1c9ddc85
CaioMiranda12 <caioclm04@gmail.com>
2024-10-13 13:04:39 -0300

    changing header links elements to buttons, and letting them funcional

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,37 @@
 import './styles.css'
 
 export default function Header() {
-    
+    function goToAbout(){
+        window.scrollTo(0, 200)
+    }
+
+    function goToTechnologies(){
+        window.scrollTo(0, 400)
+    }
+
+    function goToProjects(){
+        window.scrollTo(0, 800)
+    }
+
+    function goToTastes(){
+        window.scrollTo(0, 1500)
+    }
+
+    function goToContact(){
+        window.scrollTo(0, 1500)
+    }
 
     return (
         <header>
             <div class="content">
                 <h1>CodigoCerto</h1>
                 <div class="header-links">
-                    <a href="#">Inicio</a>
-                    <a href="#">Sobre</a>
-                    <a href="#">Projetos</a>
-                    <a href="#">Gostos Pessoais</a>
-                    <a href="#">Motivações</a>
-                    <a href="#">Contato</a>
+                    <button>Inicio</button>
+                    <button onClick={goToAbout}>Sobre</button>
+                    <button onClick={goToTechnologies}>Tecnologias</button>
+                    <button onClick={goToProjects}>Projetos</button>
+                    <button onClick={goToTastes}>Motivações</button>
+                    <button onClick={goToContact}>Contato</button>
                 </div>
             </div>
         </header>
